84f943b143e742ad2497bdde8fee5568422ccfba
symball <contact@simonball.me>
2023-03-10 15:17:21 +0800

    tiny update for tag test

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} and has been counted
         </button>
         <p>
             {t('Welcome')}
